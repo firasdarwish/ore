163fdf45821fd76500ab92638b8923f0d059a92b
Firas Darwish <f.darwish@diwink.com>
2024-02-20 12:09:58 +0300

    add benchmarks

---
+++ benchmarks_test.go
@@ -0,0 +1,73 @@
+package ore
+
+import (
+	"context"
+	"testing"
+)
+
+func BenchmarkRegisterLazyFunc(b *testing.B) {
+	clearAll()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		RegisterLazyFunc[Counter](Scoped, func(ctx context.Context) Counter {
+			return &simpleCounter{}
+		})
+	}
+}
+
+func BenchmarkRegisterLazyCreator(b *testing.B) {
+	clearAll()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		RegisterLazyCreator[Counter](Scoped, &simpleCounter{})
+	}
+}
+
+func BenchmarkRegisterEagerSingleton(b *testing.B) {
+	clearAll()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		RegisterEagerSingleton[Counter](&simpleCounter{})
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	clearAll()
+
+	RegisterLazyFunc[Counter](Scoped, func(ctx context.Context) Counter {
+		return &simpleCounter{}
+	})
+
+	RegisterEagerSingleton[Counter](&simpleCounter{})
+
+	RegisterLazyCreator[Counter](Scoped, &simpleCounter{})
+
+	ctx := context.Background()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Get[Counter](ctx)
+	}
+}
+
+func BenchmarkGetList(b *testing.B) {
+	clearAll()
+
+	RegisterLazyFunc[Counter](Scoped, func(ctx context.Context) Counter {
+		return &simpleCounter{}
+	})
+
+	RegisterEagerSingleton[Counter](&simpleCounter{})
+
+	RegisterLazyCreator[Counter](Scoped, &simpleCounter{})
+
+	ctx := context.Background()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		GetList[Counter](ctx)
+	}
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/firasdarwish/ore
-
-go 1.22.0
