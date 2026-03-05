bd3f8dd016e8bf0cd5bab79b6824b25c7eb16243
Firas Darwish <f.darwish@diwink.com>
2026-03-05 14:41:25 +0300

    fix getUnderlyingTypeName incorrectly strips * from package-qualified names for double pointers **Service

--- utils.go
+++ utils.go
@@ -39,12 +39,7 @@ func getPointerTypeName[T any]() pointerTypeName {
 }
 
 func getUnderlyingTypeName(ptn pointerTypeName) string {
-	s := string(ptn)
-	index := strings.Index(s, "*")
-	if index == -1 {
-		return s // no '*' found, return the original string
-	}
-	return s[:index] + s[index+1:]
+	return strings.TrimLeft(string(ptn), "*")
 }
 
 func (this typeID) String() string {
